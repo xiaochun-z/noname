5a0b2ac9484f7ceac1b1be87eed0acfd082ed476
xizifu <2238983941@qq.com>
2024-06-16 22:16:42 +0800

    补充评级

--- character/rank.js
+++ character/rank.js
@@ -1143,8 +1143,10 @@ window.noname_character_rank = {
 		"huan_simayi",
 		"huan_zhaoyun",
 		"huan_guojia",
+		"huan_jiangwei",
 	],
 	b: [
+		"huan_zhugeliang",
 		"huan_zhanghe",
 		"star_zhangzhao",
 		"dc_sp_zhurong",
@@ -2816,6 +2818,8 @@ window.noname_character_rank = {
 			"matie",
 			"huan_weiyan",
 			"huan_zhaoyun",
+			"huan_jiangwei",
+			"huan_zhugeliang",
 		],
 		junk: [
 			"ol_peixiu",
