94782e306a2d294a91f3feedf6f237967d6a3081
Rintim <rintim@foxmail.com>
2025-03-11 23:10:29 +0800

    fix: update type info && straighten the type of `Player` for `GameEvent` when functions of `Player` use properties of `GameEvent`.

--- noname/library/element/gameEvent.js
+++ noname/library/element/gameEvent.js
@@ -1,4 +1,5 @@
 import { _status, game, get, lib, ui } from "../../../noname.js";
+import { Player } from "./index.js";
 import security from "../../util/security.js";
 import ContentCompiler from "./GameEvent/compilers/ContentCompiler.js";
 import GameEventManager from "./GameEvent/GameEventManager.js";
