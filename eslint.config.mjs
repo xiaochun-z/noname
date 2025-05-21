3f7765725c7d2be34bb4adb47a9f1fefc04cfb2d
Rintim <rintim@foxmail.com>
2025-05-21 21:36:26 +0800

    fix: fix the style error of cards.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,6 +25,7 @@ export default ts.config(
 			"@typescript-eslint/no-require-imports": 0,
 			"@typescript-eslint/no-unused-vars": 0,
 			"@typescript-eslint/no-unused-expressions": 0,
+			"@typescript-eslint/no-this-alias": 0,
 			"no-class-assign": 0,
 			"no-console": 0,
 			"no-constant-condition": [
