9d3fc7a30c07eb8fd6bd3861d9e97c5383fe9bac
xizifu <2238983941@qq.com>
2025-05-27 19:50:17 +0800

    OL界简雍分包调整；修正onlyOL_yijiang5为OL专属·将4的分包名

--- character/onlyOL/sort.js
+++ character/onlyOL/sort.js
@@ -1,7 +1,7 @@
 const characterSort = {
-	onlyOL_yijiang1: ["ol_zhangchunhua", "ol_jianyong", "ol_lingtong", "ol_gaoshun", "ol_fazheng", "ol_wuguotai", "ol_caozhi"],
+	onlyOL_yijiang1: ["ol_zhangchunhua", "ol_lingtong", "ol_gaoshun", "ol_fazheng", "ol_wuguotai", "ol_caozhi"],
 	onlyOL_yijiang2: ["ol_liaohua", "ol_caozhang", "ol_chengpu", "ol_wangyi", "ol_liubiao", "ol_guanzhang", "ol_madai"],
-	onlyOL_yijiang3: ["ol_guohuai", "ol_fuhuanghou", "ol_yufan", "ol_liru", "ol_caochong"],
+	onlyOL_yijiang3: ["ol_jianyong", "ol_guohuai", "ol_fuhuanghou", "ol_yufan", "ol_liru", "ol_caochong"],
 	onlyOL_yijiang4: ["ol_caifuren"],
 	onlyOL_yijiang5: ["ol_sunxiu"],
 	onlyOL_ol_jsrg: ["ol_jsrg_zhaoyun", "ol_jsrg_liuhong"],
@@ -20,7 +20,7 @@ const characterSortTranslate = {
 	onlyOL_yijiang2: "OL专属·将2",
 	onlyOL_yijiang3: "OL专属·将3",
 	onlyOL_yijiang4: "OL专属·将4",
-	onlyOL_yijiang5: "OL专属·将4",
+	onlyOL_yijiang5: "OL专属·将5",
 	onlyOL_ol_jsrg: "闪耀星河·江山如故",
 	onlyOL_sb_mouding: "上兵伐谋·谋定天下",
 	onlyOL_sb_wudong: "上兵伐谋·武动乾坤",
