0fb479d694f4d334d653410b32a5a579b931ad0f
xizifu <2238983941@qq.com>
2025-05-27 16:37:24 +0800

    忠锷伍孚转正；补加遗漏的大括号

--- character/replace.js
+++ character/replace.js
@@ -412,5 +412,5 @@ window.noname_character_replace = {
 	guanhai: ["ol_guanhai", "guanhai"],
 	zerong: ["zerong", "std_zerong"],
 	tw_zhugejun: ["tw_zhugejun", "dc_zhugejun"],
-	wufu: ["wufu", "ze_wufu"],
+	wufu: ["wufu", "hr_wufu"],
 };
